8cb831a7d78608189ef87b53fc6ffc0cc08d0d3a
Satyam Rai <114270778+Satyamrai8707@users.noreply.github.com>
2026-01-15 12:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(currentSquare => {
+	currentSquare.addEventListener("mouseenter", () => {
+		squares.forEach(square => {
+			if (square !== currentSquare) {
+				square.style.backgroundColor = "#6F4E37"; // Coffee
+			}
+		});
+	});
+
+	currentSquare.addEventListener("mouseleave", () => {
+		squares.forEach(square => {
+			square.style.backgroundColor = "#E6E6FA"; // Lavender
+		});
+	});
+});
